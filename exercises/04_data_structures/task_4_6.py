2b3493daa1814c0895b67a51d8ba1dc09403c771
kasatka231 <extreme.s.k@mail.ru>
2020-01-19 19:03:52 +0000

    добавляю_решенное_4.6

--- exercises/04_data_structures/task_4_6.py
+++ exercises/04_data_structures/task_4_6.py
@@ -15,3 +15,20 @@ Outbound Interface:    FastEthernet0/0
 """
 
 ospf_route = "O        10.0.24.0/24 [110/41] via 10.0.13.3, 3d18h, FastEthernet0/0"
+#Пока достичь результата,у меня получается с помощью костыльных методов
+#Для начала я удаляю символы,которые мне мешают в последствии- это квадратные скобки и запятые
+ospf1 = ospf_route.replace(',', '').replace('[','').replace(']','')
+#print(ospf1)
+#Далее методом split получаю список strings для того,чтобы к ним можно обращаться по индексу
+ospf2 = (ospf1.split())
+#Далее использую метод format,обращаясь к разным индексам для корректного вывода.
+ip_template = '''
+Protocol:             {0:>}
+Prefix:               {1:>}
+AD/Metric             {2:>} 
+Next-Hop:             {3:>}
+Last update:          {4:>}     
+Outbound Interface:   {5:>}
+'''
+print(ip_template.format('OSPF', ospf2[1], ospf2[2], ospf2[4], ospf2[5],  ospf2[6]))
+
