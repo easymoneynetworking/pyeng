a6672203262f7ee819a289206e8623f21f0e21ac
pyneng <pyneng.course@gmail.com>
2020-01-25 18:36:17 +0000

    Загрузила решения в ветку task_check

--- exercises/05_basic_scripts/task_5_2.py
+++ exercises/05_basic_scripts/task_5_2.py
@@ -19,3 +19,34 @@ Mask:
 
 Ограничение: Все задания надо выполнять используя только пройденные темы.
 """
+
+ip_address = input('Enter the ip address in format x.x.x.x/x: ')
+
+
+ip_address1 = ip_address.replace('/','.')
+ip_address2 = ip_address1.split(".")
+ip_address3 = int(ip_address2[4])
+maska0 = 32 - ip_address3
+maska1 = '1' * ip_address3
+maska2 = '0' * maska0
+maska3 = maska1 + maska2
+maska4 = maska3[0:8] + ' ' +maska3[8:16] + ' ' +maska3[16:24] + ' ' +maska3[24:33]
+maska5 = maska4.split()
+maska9 = int(maska5[0], 2)
+maska10 = int(maska5[1], 2)
+maska11 = int(maska5[2], 2)
+maska12 = int(maska5[3], 2)
+
+output = """
+Network:
+{0:<10}{1:<10}{2:<10}{3:<10}
+{0:08b}  {1:08b}  {2:08b}  {3:08b}
+
+Mask:
+/{4}
+{9:<10}{10:<10}{11:<10}{12:<10}
+{5:<10}{6:<10}{7:<10}{8:<10}
+"""
+
+print(output.format(int(ip_address2[0]), int(ip_address2[1]), int(ip_address2[2]), int(ip_address2[3]),int(ip_address2[4]), maska5[0], maska5[1], maska5[2], maska5[3], maska9, maska10, maska11, maska12 ))
+
