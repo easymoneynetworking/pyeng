70089f2d06b5360dc80ad8993ce99ebed437606c
pyneng <pyneng.course@gmail.com>
2020-01-28 09:09:59 +0000

    Загрузила решения в ветку task_check

--- exercises/06_control_structures/task_6_2.py
+++ exercises/06_control_structures/task_6_2.py
@@ -14,3 +14,25 @@
 
 Ограничение: Все задания надо выполнять используя только пройденные темы.
 """
+
+ip_address = input('Введите IP-адреса в формате 10.0.1.1: ')
+ip_list = ip_address.split(".")
+oct1, oct2, oct3, oct4 = [
+    int(ip_list[0]),
+    int(ip_list[1]),
+    int(ip_list[2]),
+    int(ip_list[3]),
+]
+
+if oct1 in range(1, 223):
+    print('unicast')
+elif oct1 in range(224, 239):
+    print('multicast')
+elif ip_address == '255.255.255.255':
+    print('local broadcast')
+elif ip_address == '0.0.0.0':
+        print('unassigned')
+else:
+    print('unused')
+
+
