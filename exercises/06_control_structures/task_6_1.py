8a0dc24b81542587ca47dbaf3a9fd543d9e32d04
pyneng <pyneng.course@gmail.com>
2020-01-28 09:12:34 +0000

    Проверила задания 6.1, 6.2

--- exercises/06_control_structures/task_6_1.py
+++ exercises/06_control_structures/task_6_1.py
@@ -16,7 +16,11 @@ mac = ["aabb:cc80:7000", "aabb:dd80:7340", "aabb:ee80:7000", "aabb:ff80:7000"]
 mac_cisco = []
 
 for macs in mac:
+    # тут не надо применять str, так как macs строка
     new = str(macs).replace(":",".")
     mac_cisco.append(new)
 
 print('mac_cisco = {}'.format(mac_cisco))
+
+# Все отлично
+
--- exercises/06_control_structures/task_6_2.py
+++ exercises/06_control_structures/task_6_2.py
@@ -31,8 +31,27 @@ elif oct1 in range(224, 239):
 elif ip_address == '255.255.255.255':
     print('local broadcast')
 elif ip_address == '0.0.0.0':
-        print('unassigned')
+    print('unassigned')
 else:
     print('unused')
 
+
+
+# Все отлично
+
+# вариант решения
+
+ip_address = input("введите ip-адрес: ")
+oct1 = int(ip_address.split(".")[0])
+
+if ip_address == "255.255.255.255":
+    print("local broadcast")
+elif ip_address == "0.0.0.0":
+    print("unassigned")
+elif 1 <= oct1 <= 223:
+    print("unicast")
+elif 224 <= oct1 <= 239:
+    print("multicast")
+else:
+    print("unused")
 
