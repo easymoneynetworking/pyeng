8a056834c0695eb707d44580a38dc8a5d50471d1
kasatka231 <extreme.s.k@mail.ru>
2020-01-29 19:46:48 +0000

    task_6_2a.py

--- exercises/06_control_structures/task_6_2a.py
+++ exercises/06_control_structures/task_6_2a.py
@@ -16,3 +16,43 @@
 Ограничение: Все задания надо выполнять используя только пройденные темы.
 
 """
+
+ip_address = input('Введите IP-адреса в формате 10.0.1.1: ')
+
+correct_ip = True
+parts = ip_address.split(".")
+normal_address = '10.0.1.1'
+
+
+
+for item in parts:
+    if item.isdigit() == False:
+        correct_ip = False
+    elif 0 < int(item) > 255:
+        correct_ip = False
+    if len(parts) != 4:
+        correct_ip = False
+    elif normal_address.count('.') != ip_address.count('.'):
+        correct_ip = False
+
+if correct_ip == False:
+   print('Неправильный IP-адрес')
+else:
+    ip_list = ip_address.split(".")
+    oct1, oct2, oct3, oct4 = [
+        int(ip_list[0]),
+        int(ip_list[1]),
+        int(ip_list[2]),
+        int(ip_list[3]),
+        ]
+    if oct1 in range(1, 223):
+        print('unicast')
+    elif oct1 in range(224, 239):
+        print('multicast')
+    elif ip_address == '255.255.255.255':
+        print('local broadcast')
+    elif ip_address == '0.0.0.0':
+        print('unassigned')
+    else:
+        print('unused')
+
