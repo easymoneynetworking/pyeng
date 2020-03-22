3201729cddb455ca1a0b79483690800c1659c1ed
kasatka231 <extreme.s.k@mail.ru>
2020-03-22 12:47:58 +0000

    finish 19.1b

--- exercises/19_ssh_telnet/task_19_1b.py
+++ exercises/19_ssh_telnet/task_19_1b.py
@@ -11,3 +11,28 @@
 
 Для проверки измените IP-адрес на устройстве или в файле devices.yaml.
 """
+from netmiko import ConnectHandler, NetmikoAuthenticationException, NetmikoTimeoutException
+
+import yaml
+from pprint import pprint
+from netmiko import ConnectHandler
+#command = "sh ip int br"
+
+def send_show_command(device,command):
+    try:
+        with ConnectHandler(**device) as ssh:
+            ssh.enable()
+            result = ssh.send_command(command)
+        return result
+    except NetmikoAuthenticationException:
+        return print('Authentication failure: unable to connect')
+    except NetmikoTimeoutException:
+        return print('Connection to device timed-out')
+
+if __name__ == "__main__":
+    command = "sh ip int br"
+    with open('devices.yaml') as f:
+        devices = yaml.safe_load(f)
+        for dev in devices:
+            pprint(send_show_command(dev,command))
+
