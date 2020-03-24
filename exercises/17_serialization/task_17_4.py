b4e778bce9857341535a5a47b71ae6a1215e7b55
pyneng <pyneng.course@gmail.com>
2020-03-24 07:47:19 +0000

    Проверила задания 17.3a 17.3b 17.4

--- exercises/17_serialization/task_17_4.py
+++ exercises/17_serialization/task_17_4.py
@@ -76,3 +76,29 @@ if __name__ == "__main__":
     write_last_log_to_csv('mail_log.csv','ma_last_log.csv')
 
 
+
+# Все отлично
+
+# вариант решения
+import csv
+
+
+def write_last_log_to_csv(source_log, output):
+    with open(source_log) as f:
+        data = list(csv.reader(f))
+        header = data[0]
+    result = {}
+    sorted_by_date = sorted(
+        data[1:], key=lambda x: convert_datetimestr_to_datetime(x[2])
+    )
+    for name, email, date in sorted_by_date:
+        result[email] = (name, email, date)
+    with open(output, "w") as dest:
+        writer = csv.writer(dest)
+        writer.writerow(header)
+        for row in result.values():
+            writer.writerow(row)
+
+
+if __name__ == "__main__":
+    write_last_log_to_csv("mail_log.csv", "example_result.csv")
