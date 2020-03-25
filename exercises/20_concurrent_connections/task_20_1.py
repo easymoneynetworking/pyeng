86e3a165cae5b3a03afd9a6a05b0a24e40265533
kasatka231 <extreme.s.k@mail.ru>
2020-03-25 13:25:42 +0000

    finish 20.1

--- exercises/20_concurrent_connections/task_20_1.py
+++ exercises/20_concurrent_connections/task_20_1.py
@@ -17,3 +17,37 @@
 
 Для проверки доступности IP-адреса, используйте ping.
 """
+
+import subprocess
+from pprint import pprint
+from concurrent.futures import ThreadPoolExecutor
+import logging
+
+def check_avalaibility(ip_address):
+        reply = subprocess.run(['ping', '-c', '1', '-n', ip_address],
+        stdout=subprocess.DEVNULL)
+        if reply.returncode == 0:
+            return True
+        else:
+            return False
+
+
+def ping_ip_addresses(ip_address,limit=3):
+    get_reach = []
+    get_unreach = []
+    with ThreadPoolExecutor(max_workers=limit) as executor:
+        result = executor.map(check_avalaibility, ip_address)
+        for code,address in zip(result, ip_address):
+            if code:
+                get_reach.append(address)
+            else:
+                get_unreach.append(address)
+    return get_reach, get_unreach
+
+if __name__ == '__main__':
+    lists_ip = ["1.1.1", "8.8.8.8", "8.8.4.4", "8.8.7.1"]
+    pprint(ping_ip_addresses(lists_ip,3))
+
+
+
+
