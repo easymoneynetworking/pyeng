c8ae0263f475ab627c84034cb899d79423175073
kasatka231 <extreme.s.k@mail.ru>
2020-04-01 21:25:19 +0000

    finish 22.1a

--- exercises/22_textfsm/task_22_1a.py
+++ exercises/22_textfsm/task_22_1a.py
@@ -14,3 +14,25 @@
 
 Проверить работу функции на выводе команды output/sh_ip_int_br.txt и шаблоне templates/sh_ip_int_br.template.
 """
+from pprint import pprint
+import textfsm
+
+
+def parse_output_to_dict(template, command_output):
+    finish_dic = {}
+    with open(template) as f:
+        re_table = textfsm.TextFSM(f)
+        header = re_table.header
+        result = re_table.ParseText(command_output)
+        return [dict(zip(header, i)) for i in result]
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    with open('output/sh_ip_int_br.txt') as output:
+        outputs = output.read()
+        pprint(parse_output_to_dict('templates/sh_ip_int_br.template', outputs))
